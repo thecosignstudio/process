2fea70e9ff190000773938b4e7aa7b37a53301bb
adam-szczombrowski <adam.szczombrowski@gmail.com>
2017-06-21 13:00:27 +0200

    hide application sharer on click wherever

--- javascript/application.js
+++ javascript/application.js
@@ -2,7 +2,14 @@ $( document ).ready(function() {
   $("#toc").toc({content: '.nav', headings: 'h1,h2,h3,h4'});
 
   prepareNavigation();
+  hideApplicationSharer();
 });
+
+function hideApplicationSharer() {
+  $(window).click(function(event) {
+    $('#selectionSharerPopover').hide();
+  });
+}
 
 function prepareNavigation() {
   $('#toc').find('a').each(function(){
