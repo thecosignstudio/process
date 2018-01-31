c12109a2c56602b77a205156be83af728621d1ef
Sławomir Kołodziej <slawkolodziej@gmail.com>
2018-01-31 15:27:11 +0100

    Refactor

--- javascript/app/utils/chapter-navigation.js
+++ javascript/app/utils/chapter-navigation.js
@@ -21,7 +21,7 @@ function addToc() {
   });
 }
 
-function handleChapterToggle() {
+function handleChapterAnimation() {
   const $navContainer = $('.subnav');
   const $toggleHandlers = $('.chapter-toggle, .chapter-nav__listing ul li a');
   const $chaptersContainer = $('.chapter-nav__listing');
@@ -36,62 +36,51 @@ function handleChapterToggle() {
   const currentChapterAnimationDuration = 600;
   const moveCurrentChapterAnimationDuration = 300;
 
-  const moveCurrentChapter = (isForwardAnimation) => {
-    if (!isForwardAnimation) {
-      $currentChapterContainer.css({
-        top: navContainerTopPadding,
-        left: 'initial',
-        marginLeft: 0,
-        width: '100%',
-      });
-      return setTimeout(() => $currentChapterContainer.css('position', 'static'), moveCurrentChapterAnimationDuration);
-    }
-
-    $currentChapterContainer.css({
-      position: 'absolute',
-      left: 0,
-      marginLeft: navContainerLeftPadding,
-      width: `calc(100% - ${navContainerLeftPadding} - 15px`,
-    });
-    const chapterPositionInNav = $chapters.reduce((prevValue, chapter) => {
-      if ($(chapter).find('.chapter-number').text() === chapterNumber) {
-        return $(chapter).position().top - chapterTransformY;
+  const moveCurrentChapter = (delay, isForwardAnimation) => {
+    setTimeout(() => {
+      if (!isForwardAnimation) {
+        $currentChapterContainer.css({
+          top: navContainerTopPadding,
+          left: 'initial',
+          marginLeft: 0,
+          width: '100%',
+        });
+        return setTimeout(() => $currentChapterContainer.css('position', 'static'), moveCurrentChapterAnimationDuration);
       }
-      return prevValue;
-    }, null);
-    return $currentChapterContainer.css('top', chapterPositionInNav);
+
+      $currentChapterContainer.css({
+        position: 'absolute',
+        left: 0,
+        marginLeft: navContainerLeftPadding,
+        width: `calc(100% - ${navContainerLeftPadding} - 15px`,
+      });
+      const chapterPositionInNav = $chapters.reduce((prevValue, chapter) => {
+        if ($(chapter).find('.chapter-number').text() === chapterNumber) {
+          return $(chapter).position().top - chapterTransformY;
+        }
+        return prevValue;
+      }, null);
+      return $currentChapterContainer.css('top', chapterPositionInNav);
+    }, delay);
   };
 
-  $toggleHandlers.click(() => {
-    if ($navContainer.hasClass('animating')) return;
-
-    const chaptersWithoutCurrent = $chapters.filter(chapter => (
-      $(chapter).find('.chapter-number').text() !== chapterNumber
-    ));
-    const isForwardAnimation = $currentChapterContainer.hasClass('subnav__chapters-container--active');
-
-    const currentChapterAnimationDelay = isForwardAnimation ? 0 :
-      singleChapterAnimationDuration + moveCurrentChapterAnimationDuration;
-    const moveAnimationDelay = isForwardAnimation ? currentChapterAnimationDuration : 0;
-    const paddingAnimationDelay = isForwardAnimation ? currentChapterAnimationDuration : singleChapterAnimationDuration;
-    const chaptersAnimationDelay = isForwardAnimation ?
-      currentChapterAnimationDuration + moveCurrentChapterAnimationDuration : 0;
-
-    $navContainer.addClass('animating');
+  function animateChapters(delay) {
     setTimeout(() => {
-      $currentChapterContainer.toggleClass('subnav__chapters-container--active');
-      setTimeout(() => {
-        $chaptersContainer.toggleClass('chapter-nav__listing--active');
-      }, currentChapterAnimationDuration);
-      setTimeout(() => {
-        $navContainer.toggleClass('subnav--small-padding');
-      }, paddingAnimationDelay);
-      setTimeout(() => {
-        moveCurrentChapter(isForwardAnimation);
-      }, moveAnimationDelay);
-    }, currentChapterAnimationDelay);
+      $chaptersContainer.toggleClass('chapter-nav__listing--active');
+    }, delay);
+  }
 
+  function switchNavPadding(delay) {
     setTimeout(() => {
+      $navContainer.toggleClass('subnav--small-padding');
+    }, delay);
+  }
+
+  function animateCurrentChapterItems(delay, isForwardAnimation) {
+    setTimeout(() => {
+      const chaptersWithoutCurrent = $chapters.filter(chapter => (
+        $(chapter).find('.chapter-number').text() !== chapterNumber
+      ));
       if (!isForwardAnimation) {
         $(chaptersWithoutCurrent).removeClass('visible');
         $navContainer.removeClass('animating');
@@ -105,11 +94,35 @@ function handleChapterToggle() {
           }, index * singleChapterAnimationDuration)
         ));
       }
-    }, chaptersAnimationDelay);
+    }, delay);
+  }
+
+  $toggleHandlers.click(() => {
+    if ($navContainer.hasClass('animating')) return;
+
+    const isForwardAnimation = $currentChapterContainer.hasClass('subnav__chapters-container--active');
+
+    const currentChapterAnimationDelay = isForwardAnimation ? 0 :
+      singleChapterAnimationDuration + moveCurrentChapterAnimationDuration;
+    const moveAnimationDelay = isForwardAnimation ? currentChapterAnimationDuration : 0;
+    const paddingSwitchDelay = isForwardAnimation ?
+      currentChapterAnimationDuration : singleChapterAnimationDuration;
+    const currentChapterItemsAnimationDelay = isForwardAnimation ?
+      currentChapterAnimationDuration + moveCurrentChapterAnimationDuration : 0;
+
+    $navContainer.addClass('animating');
+    setTimeout(() => {
+      $currentChapterContainer.toggleClass('subnav__chapters-container--active');
+      animateChapters(currentChapterAnimationDuration);
+      switchNavPadding(paddingSwitchDelay);
+      moveCurrentChapter(moveAnimationDelay, isForwardAnimation);
+    }, currentChapterAnimationDelay);
+
+    animateCurrentChapterItems(currentChapterItemsAnimationDelay, isForwardAnimation);
   });
 }
 
 export default function () {
   addToc();
-  handleChapterToggle();
+  handleChapterAnimation();
 }
