3cdd01a233ca06aa602b74e18b6d651e7f6b4c47
Jacek Jakubik <jakubik.jacek@gmail.com>
2018-04-03 15:54:58 +0200

    Add a cachebuster to css/js

---
+++ _plugins/cachebuster.rb
@@ -0,0 +1,25 @@
+require 'digest'
+
+module Jekyll
+  # Jekyll assets cachebuster filter
+  #
+  # Place this file into `_plugins`.
+  module CachebusterFilter
+    # Usage example:
+    #
+    # {{ "/style.css" | cachebuster }}
+    # {{ "/style.css" | cachebuster | absolute_url }}
+    def cachebuster(filename)
+      sha256 = Digest::SHA256.file(
+        File.join(@context.registers[:site].dest, filename)
+      )
+
+      "#{filename}?#{sha256.hexdigest[0, 6]}"
+    rescue
+      # Return filename unmodified if file was not found
+      filename
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::CachebusterFilter)
